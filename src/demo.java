e2c5e65a0f44c42abbe1bb388f8b6339dff0ad20
kai <d0239601@fcu.edu.tw>
2015-12-21 09:56:28 +0800

    test version2

--- src/demo.java
+++ src/demo.java
@@ -1,9 +1,22 @@
-class Question {
+public class demo{
+	public static void main(String[] args ){
+		
+	}
+}
+
+abstract class test{
+	abstract void show();
+}
+class Question extends test{
    String description;
    public Question(String desc) {
       this.description = desc;
    }
    public void print() {
       System.out.println(desc);
+   }
+   void show() {
+	// TODO Auto-generated method stub
+	
    }      
 }
